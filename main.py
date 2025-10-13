73d3ca8ba629477597b2090a08b9620bd850293b
JuanManuelvm <juan.vargas.munoz@correounivalle.edu.co>
2025-10-13 18:38:08 -0500

    Version 2 del FBruta

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from input_output.parser import load_data_from_file, parse_data
 from Voraz.rocV import rocV
-from input_output.salida import calcularInsatisfaccionGeneral
+from FuerzaBruta.rocFB import rocFB
+from input_output.salida import calcularInsatisfaccionGeneral, guardar_resultados
 
 def main():
-    data = load_data_from_file('Pruebas/Prueba46.txt')
+    data = load_data_from_file('Pruebas/Prueba1.txt')
     materias, estudiantes = parse_data(data)
     #print(materias, estudiantes)
+
+    satisfacion, solucion = rocFB(materias, estudiantes)
+    guardar_resultados("Resultados/FBruta", "Resultado1.txt", solucion, satisfacion)
+    print(f'Insatisfacción General: {satisfacion}')
+    print(f'Solución: {solucion}')
+
     materiasAsignadas = rocV(materias, estudiantes)
     print(materiasAsignadas)
     insatisfaccionGeneral = calcularInsatisfaccionGeneral(materiasAsignadas, estudiantes, materias)
